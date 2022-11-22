4f3aa569f738ee9c13da9d22733c44883f41ed34
1 <1@STUD.UNV.SU>
2022-11-22 10:07:02 +0300

    lab_4.1/lab_4.2

---
+++ Lab_4.1/Lab_4.1.cpp
@@ -0,0 +1,197 @@
+﻿#include <iostream>
+#include <cstring>
+int sizemas = 0;
+void encrypt(char* std_for_encrypt, int key) {
+	int size = strlen(std_for_encrypt);
+	for (int i = 0; i < size; i++) {
+		if (std_for_encrypt[i] != ' ') {
+			std_for_encrypt[i] = std_for_encrypt[i] + (key % 26); //цикличность
+		}
+		else {
+			std_for_encrypt[i] = ' ';
+		}
+	}
+}
+bool Palindrom(char* str) {
+	int size = strlen(str);
+	bool flag = true;
+	for (int i = 0, j = size - 1; i < size, i < j; i++, j--) {
+		if (str[i] >= 'A' and str[i] <= 'Z') {
+			str[i] += 'a' - 'A';
+		}
+		if (str[j] >= 'A' and str[j] <= 'Z') {
+			str[j] += 'a' - 'A';
+		}
+		if (str[i] != str[j]) {
+			flag = false;
+			std::cout << flag << std::endl;
+			break;
+		}
+
+	}
+	return flag;
+}
+int find_substring1(const char* str_for_search_in, const char* substring, int start_position) {
+	int size = strlen(str_for_search_in);
+	int size1 = strlen(substring);
+	int j = 0;
+	for (int i = start_position; i < size; i++) {
+		if (str_for_search_in[i] == substring[j]) {
+			j++;
+		}
+		else {
+			j = 0;
+		}
+		if (j == size1 - 1) {
+			return i - j + 1;
+		}
+	}
+	return 0;
+}
+int* find_substring2(const char* str_for_search_in, const char* substring) {
+	sizemas = 0;
+	int size = strlen(str_for_search_in);
+	int size1 = strlen(substring) + 1;
+	int j = 0;
+	int q = 0;
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		if ((str_for_search_in[i] == substring[j]) and (j < size1)) {
+			j++;
+		}
+		else {
+			j = 0;
+		}
+		if (j == size1 - 1) {
+			sizemas++;
+			arr[q] = i - size1 + 1;
+			q++;
+			j = 0;
+		}
+	}
+	return arr;
+}
+void names(char* str) {
+	bool flag = false;
+	int size = strlen(str);
+	char a[255] = { '\0' };
+	int j = 0;
+	for (int i = 0; i < size; i++) {
+		if (flag == true and str[i] != '\"' and str[i] != '<' and str[i] != '>') {
+			a[j] = str[i];
+			j++;
+		}
+		if (str[i] == '\"' or str[i] == '<' or str[i] == '>') {
+			if (flag == true) {
+				std::cout << " | ";
+				for (int q = 0; q < j; q++) {
+					std::cout << a[q];
+				}
+				j = 0;
+				char a[255] = { '\0' };
+				flag = false;
+			}
+			else {
+				flag = true;
+			}
+		}
+	}
+	std::cout << " |";
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int choice = 0;
+	while (true) {
+		std::cout << "\nЧто вы хотите выполнить? \n"
+			<< "1. Проверка на полиндром\n"
+			<< "2. Поиск подстроки в строке\n"
+			<< "3. Шифрование сдвигами (шифр Цезаря)\n"
+			<< "4. Введите текст, в котором нужно найти названия\n"
+			<< "5. Выход\n";
+		std::cin >> choice;
+		switch (choice) {
+		case 1: {
+			//start Polyndrom
+			char pol[255] = { '\0' };
+			std::cout << "Введите строку для проверки её на полиндром: ";
+			std::cin.ignore();
+			std::cin.getline(pol, 255);
+			if (Palindrom(pol) == 1) {
+				std::cout << "Palindrom\n";
+			}
+			else {
+				std::cout << "Not Palindrom\n";
+			}
+			//A Santa dog lived as a devil god at NASA
+			break;
+			//end Polyndrom
+		}
+		case 2: {
+			//start find_substring1
+			std::cout << "Введите строку, в которой будем осуществлять поиск: ";
+			char str_for_search_in[255] = { '\0' };
+			std::cin.ignore();
+			std::cin.getline(str_for_search_in, 255);
+			std::cout << "Введите строку, которую будем искать: ";
+			char substring[255];
+			std::cin.getline(substring, 255);
+			std::cout << "Введите начальную позицию: ";
+			int start;
+			std::cin >> start;
+			if (find_substring1(str_for_search_in, substring, start) == 0) {
+				std::cout << "Вхождения не найдено\n";
+			}
+			else {
+				std::cout << "Индекс: " << find_substring1(str_for_search_in, substring, start) << '\n';
+			}
+			//end find_substring1
+			//start find_substring2
+			std::cout << "Введите строку, в которой будем осуществлять поиск: ";
+			char str_for_search_in1[255] = { '\0' };
+			std::cin.ignore();
+			std::cin.getline(str_for_search_in1, 255);
+			std::cout << "Введите строку, которую будем искать: ";
+			char substring1[255];
+			std::cin.ignore();
+			std::cin.getline(substring1, 255);
+			std::cout << "Индексы вхождения:";
+			if (find_substring2(str_for_search_in1, substring1) != 0) {
+				int* pointer = find_substring2(str_for_search_in1, substring1);
+				for (int i = 0; i < sizemas; i++) {
+					std::cout << pointer[i] << " ";
+				}
+			}
+			else {
+				std::cout << "Вхождения не найдено";
+			}
+			break;
+			//end find_substring2
+		}
+		case 3: {
+			//start encrypt
+			char text[255];
+			int key;
+			std::cout << "Введите строку для шифровки: ";
+			std::cin.ignore();
+			std::cin.getline(text, 255);
+			std::cout << '\n' << "Введите сдвиг: ";
+			std::cin >> key;
+			encrypt(text, key);
+			std::cout << '\n' << text;
+			break;
+			//end encrypt*/
+		}
+		case 4: {
+			char str[255];
+			std::cout << "Введите строку: ";
+			std::cin.ignore();
+			std::cin.getline(str, 255);
+			names(str);
+			break;
+		}
+		default: {
+			return 0;
+		}
+		}
+	}
+}
---
+++ Lab_4.2/Lab_4.2.cpp
@@ -0,0 +1,266 @@
+﻿#include <iostream>
+#include <string>
+#include <fstream>
+
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::ifstream;
+using std::ofstream;
+using std::ios;
+
+void menu() {
+	string func[7] = { "File format", "File name", "File path", "File disk", "File rename", "File copy", "Exit" };
+	cout << "+---------------+\n";
+	for (int i = 0; i < 7; i++) {
+		cout << "|" << i + 1 << ". " << func[i] << "\t|" << "\n";
+		if (i != 6) {
+			cout << "|---------------|\n";
+		}
+	}
+	cout << "+---------------+\n";
+	cout << "\nEnter the action number: ";
+}
+
+bool check_path(const string str) {
+	if (str.find('.') == str.npos)
+	{
+		return false;
+	}
+	else
+	{
+		return true;
+	}
+}
+
+void cut_off_quotes(string& str) {
+	if (str[0] == '\'' || str[0] == '"')
+	{
+		str = str.substr(1, str.length());
+	}
+
+	if (str[str.length() - 1] == '\'' || str[str.length() - 1] == '"')
+	{
+		str = str.substr(0, str.length() - 1);
+	}
+}
+
+void replace_slash(string& str) {
+	for (size_t i = 0; i < str.length(); i++)
+	{
+		if (str[i] == '/')
+		{
+			str[i] = '\\';
+		}
+	}
+}
+
+string file_format(const string str) {
+	size_t str_len = str.length(), dot = str.rfind('.');
+	string f_format;
+
+	f_format = str.substr(dot + 1, str_len - dot - 1);
+
+	return f_format;
+}
+
+string file_name(const string str) {
+	size_t str_len = str.length(), dot = str.rfind('.'), r_slh = str.rfind('\\');
+	string f_name;
+
+	f_name = str.substr(r_slh + 1, dot - r_slh - 1);
+
+	return f_name;
+}
+
+string file_path(const string str) {
+	size_t str_len = str.length(), l_slh = str.find('\\'), r_slh = str.rfind('\\');
+	string f_path;
+
+	f_path = str.substr(l_slh + 1, r_slh - l_slh - 1);
+
+	return f_path;
+}
+
+char file_disk(const string str) {
+	return str[0];
+}
+
+bool file_rename(string& str, const string new_name) {
+	size_t str_len = str.length(), r_slh = str.rfind('\\'), dot = str.rfind('.');
+	string f_rename;
+
+	str = str.substr(0, r_slh + 1) + new_name + str.substr(dot, str_len - dot);
+
+	return true;
+}
+
+string file_name_copy(const string str) {
+	size_t str_len = str.length(), dot = str.rfind('.');
+	string copy_name;
+
+	copy_name = str.substr(0, dot) + "_copy" + str.substr(dot, str.length() - dot);
+
+	return copy_name;
+}
+
+bool file_copy(const string str) {
+	string copy_name = file_name_copy(str);
+	ifstream in(str, ios::in | ios::binary);
+	ofstream out(copy_name, ios::in | ios::trunc | ios::binary);
+	char buf = '\0';
+
+	if (!in) {
+		cout << "\x1B[31m\nFile \"" << str.substr(str.rfind('\\') + 1, str.length() - str.rfind('\\')) << "\" doesn't exist\n\n\033[0m";;
+		return false;
+	}
+	if (!out) {
+		cout << "\x1B[31m\nFailed to create file \"" << copy_name << "\"\n\n\033[0m";
+		return false;
+	}
+
+	while (in) {
+		in.get(buf);
+		if (in)
+		{
+			out.put(buf);
+		}
+	}
+
+	in.close();
+	out.close();
+	return true;
+}
+
+int main() {
+	int choise = 0;
+	string str, new_name;
+	bool result = false;
+	while (true)
+	{
+		menu();
+		cin >> choise;
+		switch (choise)
+		{
+		case 1: {
+			cout << "\nEnter the file path: ";
+			cin.ignore(1, '\n');
+			getline(cin, str);
+			cut_off_quotes(str);
+			replace_slash(str);
+
+			if (check_path(str))
+			{
+				cout << "\nFile format is \"\x1B[32m" << file_format(str) << "\033[0m\"\n\n";
+			}
+			else
+			{
+				cout << "\x1B[31m\nInvalid path entered\n\n\033[0m";
+			}
+			break;
+		}
+		case 2: {
+			cout << "\nEnter the file path: ";
+			cin.ignore(1, '\n');
+			getline(cin, str);
+			cut_off_quotes(str);
+			replace_slash(str);
+
+			if (check_path(str))
+			{
+				cout << "\nFile name is \"\x1B[32m" << file_name(str) << "\033[0m\"\n\n";
+			}
+			else
+			{
+				cout << "\x1B[31m\nInvalid path entered\n\n\033[0m";
+			}
+			break;
+		}
+		case 3: {
+			cout << "\nEnter the file path: ";
+			cin.ignore(1, '\n');
+			getline(cin, str);
+			cut_off_quotes(str);
+			replace_slash(str);
+
+			if (check_path(str))
+			{
+				cout << "\nFile path is \"\x1B[32m" << file_path(str) << "\033[0m\"\n\n";
+			}
+			else
+			{
+				cout << "\x1B[31m\nInvalid path entered\n\n\033[0m";
+			}
+			break;
+		}
+		case 4: {
+			cout << "\nEnter the file path: ";
+			cin.ignore(1, '\n');
+			getline(cin, str);
+			cut_off_quotes(str);
+			replace_slash(str);
+
+			if (check_path(str))
+			{
+				cout << "\nFile disk is \"\x1B[32m" << file_disk(str) << "\033[0m\"\n\n";
+			}
+			else
+			{
+				cout << "\x1B[31m\nInvalid path entered\n\n\033[0m";
+			}
+			break;
+		}
+		case 5: {
+			cout << "\nEnter the file path: ";
+			cin.ignore(1, '\n');
+			getline(cin, str);
+			cut_off_quotes(str);
+			replace_slash(str);
+
+			cout << "\nEnter new file name: ";
+			getline(cin, new_name);
+
+			if (check_path(str))
+			{
+				file_rename(str, new_name);
+				cout << "\nNew file path is \"\x1B[32m" << str << "\033[0m\"\n\n";
+			}
+			else
+			{
+				cout << "\x1B[31m\nInvalid path entered\n\n\033[0m";
+			}
+			break;
+		}
+		case 6: {
+			cout << "\nEnter the file path: ";
+			cin.ignore(1, '\n');
+			getline(cin, str);
+			cut_off_quotes(str);
+			replace_slash(str);
+
+			if (check_path(str))
+			{
+				if (file_copy(str))
+				{
+					cout << "\x1B[32m\nFile copy was successful\n\n\033[0m";
+				}
+				else
+				{
+					cout << "\x1B[31m\nFile copy was unsuccessful\n\n\033[0m";
+				}
+			}
+			else
+			{
+				cout << "\x1B[31m\nInvalid path entered\n\n\033[0m";
+			}
+			break;
+		}
+		case 7:
+			exit(0);
+			break;
+		default:
+			break;
+		}
+	}
+}
