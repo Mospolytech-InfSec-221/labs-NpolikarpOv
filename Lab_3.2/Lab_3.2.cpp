8b849abd92eca7a039c709e63d4353427be77569
NpolikarpOv <npolikarpov041@gmail.com>
2023-01-19 15:12:56 +0400

    exam update

--- Lab_3.2/Lab_3.2.cpp
+++ Lab_3.2/Lab_3.2.cpp
@@ -1,75 +1,101 @@
 ﻿#include <iostream>
-
-void input(int*& arr, int& len)
-{
-	std::cout << "input massive len:";
-	std::cin >> len;
-	if (arr != nullptr)
-	{
+void input(int*& arr, int& size) {
+	if (arr != nullptr) {
 		delete[] arr;
 	}
-	arr = new int[len];
-	for (int i = 0; i < len; i++)
-	{
+	std::cout << "Введите размер массива: ";
+	std::cin >> size;
+	arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		std::cout << "Введите число № " << i + 1 << "-> ";
 		std::cin >> arr[i];
 	}
 }
-
-void output(int* arr, int len)
-{
-	if (arr != nullptr)
-	{
-		for (int i = 0; i < len; i++)
-		{
-			std::cout << arr[i] << ' ';
-		}
-		std::cout << '\n';
+void output(int* arr, int n) {
+	std::cout << "{ ";
+	for (int i = 0; i < n; i++) {
+		std::cout << arr[i] << " ";
 	}
-	else
-	{
-		std::cout << "array is empty\n\n";
-	}
+	std::cout << "}";
 }
-
-int num_len(int num, int count)
+int funcsum(int a1)
 {
-	int n = 10;
-	while (num >= n)
-	{
-		count++;
-		n *= 10;
+	int sum1 = 0;
+	while (a1 != 0) {
+		sum1 += (a1 % 100) % 10;
+		a1 /= 100;
 	}
-	return(count);
+	return sum1;
 }
-
-int sum_digit(int num)
-{
-	int count = 0, sum = 0, n = 10;
-	count = num_len(num, count);
-	if (count % 2 == 0)
-	{
-		num /= 10;
-	}
-	for (int i = 0; i < count; i++)
-	{
-		sum += num % 10;
-		num /= 100;
-	}
-	return(sum);
-}
-
-void sort(int* arr, int len)
-{
-	int k = 0;
-	for (int i = 0; i < len; i++)
-	{
-		for (int j = 0; j < len - i - 1; j++)
-		{
-			if (sum_digit(arr[j]) > sum_digit(arr[j + 1]))
-			{
-				k = arr[j];
+int sort1(int* arr, int k) {
+	std::cout << std::endl;
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < k - 1; j++) {
+			if (funcsum(arr[j]) > funcsum(arr[j + 1])) {
+				int b = arr[j];
 				arr[j] = arr[j + 1];
-				arr[j + 1] = k;
+				arr[j + 1] = b;
 			}
 		}
 	}
+	std::cout << "Сортировка выполнена \n";
+	return *arr;
+}
+int sort2(int* arr, int k) {
+	std::cout << std::endl;
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < k - 1; j++) {
+			if (arr[j] % 10 > arr[j + 1] % 10) {
+				int b = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = b;
+			}
+		}
+	}
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < k - 1; j++) {
+			if (arr[j] < arr[j + 1] and arr[j] % 10 == arr[j + 1] % 10) {
+				int b = arr[j];
+				arr[j] = arr[j + 1];
+				arr[j + 1] = b;
+			}
+		}
+	}
+	return *arr;
+}
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int choice, n;
+	int* arr = nullptr;
+	while (true) {
+		std::cout << "Что вы хотите выполнить? \n"
+			<< "1. Ввод массива\n"
+			<< "2. Вывод массива\n"
+			<< "3. Сортировка по сумме цифр, стоящих на чётных местах\n"
+			<< "4. Сортировка массива вначале по возрастанию последней цифры, а при сопадении последних цирф - по убыванию\n"
+			<< "5. Выход\n";
+		std::cin >> choice;
+		switch (choice) {
+		case 1: {
+			input(arr, n);
+			break;
+		}
+		case 2: {
+			output(arr, n);
+			break;
+		}
+		case 3: {
+			sort1(arr, n);
+			break;
+		}
+		case 4: {
+			sort2(arr, n);
+			break;
+		}
+		default: {
+			delete[] arr;
+			return 0;
+		}
+		}
+	}
+}
--- Polikarpov lab 3.1/Polikarpov lab 3.1.cpp
+++ Polikarpov lab 3.1/Polikarpov lab 3.1.cpp
@@ -1,179 +1,140 @@
 ﻿#include <iostream>
-#include <string>
-using std::cout;
-using std::cin;
-using std::endl;
-
-void bubble(int* arr, int i) {
-	for (int k = 0; k < i - 1; k++) {
-		for (int j = 0; j < i - 2; j++) {
+void bubbles(int* arr, int k) {
+	for (int i = 0; i < k; i++) {
+		for (int j = 0; j < k - 1; j++) {
 			if (arr[j] > arr[j + 1]) {
-				int change = arr[j];
+				int b = arr[j];
 				arr[j] = arr[j + 1];
-				arr[j + 1] = change;
+				arr[j + 1] = b;
 			}
 		}
 	}
+	std::cout << "{ ";
+	for (int i = 0; i < k; i++) {
+		std::cout << arr[i] << " ";
+	}
+	std::cout << '}' << '\n';
+
 }
 
-void counter(char* arr1, int a) {
-	const int elem = 26;
-	int arr[elem] = { 0 };
-	cout << "\n";
-	for (int i = 0; i < a; i++) {
-		int j = arr1[i] - 'a';
-		arr[j]++;
+void withcounter(char* arr1, int size) {
+	int q = 0, pod[26] = { 0 }, size_alp = 26;
+	for (int i = 0; i < size; i++) {
+		q = arr1[i] - 'a';
+		pod[q]++;
 	}
-	int i = 0;
-	int j = 0;
-	while (j <= elem) {
-		if (arr[j] > 0) {
-			arr1[i] = 'a' + j;
+	int i = 0, r = 0;
+	while (r <= size_alp) {
+		if (pod[r] > 0) {
+			arr1[i] = char(int('a') + r);
 			i++;
-			arr[j]--;
+			pod[r]--;
 		}
 		else {
-			j++;
+			r++;
 		}
 	}
+	std::cout << "{ ";
+	for (int i = 0; i < size; i++) {
+		std::cout << arr1[i] << " ";
+	}
+	std::cout << "}\n";
 }
-
-void merge(int* a, int n)
+void merge(int*, int, int, int); //объявляем функцию merge, так как она используется в merge_sort
+void merge_sort(int* arr, int low, int high)
+//изначально делим пополам, потом половинку на 2 (получаем четверть) до тех пор пока не получим единчиный массив, что указано в условии
 {
-	int mid = n / 2; // находим середину сортируемой последовательности
-	if (n % 2 == 1)
-		mid++;
-	int h = 1; // шаг
-	// выделяем память под формируемую последовательность
-	int* c = (int*)malloc(n * sizeof(int));
-	int step;
-	while (h < n)
-	{
-		step = h;
-		int i = 0;   // индекс первого пути
-		int j = mid; // индекс второго пути
-		int k = 0;   // индекс элемента в результирующей последовательности
-		while (step <= mid)
-		{
-			while ((i < step) && (j < n) && (j < (mid + step)))
-			{ // пока не дошли до конца пути
-			  // заполняем следующий элемент формируемой последовательности
-			  // меньшим из двух просматриваемых
-				if (a[i] < a[j])
-				{
-					c[k] = a[i];
-					i++; k++;
-				}
-				else {
-					c[k] = a[j];
-					j++; k++;
-				}
-			}
-			while (i < step)
-			{ // переписываем оставшиеся элементы первого пути (если второй кончился раньше)
-				c[k] = a[i];
-				i++; k++;
-			}
-			while ((j < (mid + step)) && (j < n))
-			{  // переписываем оставшиеся элементы второго пути (если первый кончился раньше)
-				c[k] = a[j];
-				j++; k++;
-			}
-			step = step + h; // переходим к следующему этапу
-		}
-		h = h * 2;
-		// Переносим упорядоченную последовательность (промежуточный вариант) в исходный массив
-		for (i = 0; i < n; i++)
-			a[i] = c[i];
+	int mid;
+	if (low < high) {
+		mid = (low + high) / 2; // середина ряда
+		merge_sort(arr, low, mid); // рекурсивно передвигаем вверхнюю границу до серидины (то есть делим массив на 2 и берём первую часть)
+		merge_sort(arr, mid + 1, high); // рекурсивно передвигаем нижнюю границу (то есть делим массив на 2 и берём вторую половину)
+		merge(arr, low, high, mid); // сортируем и заносим значения в глобальный массив arr2
 	}
 }
-
-
+void merge(int* arr, int low, int high, int mid)
+{
+	int i, j, k, c[50];
+	//i и j образуют неотсортированную пару
+	i = low; // счётчик для нижней границы
+	k = low; //индекс для основного массива
+	j = mid + 1; // счётчик для нижней границы относительно середины mid + 1
+	while (i <= mid and j <= high) { // если нижняя граница меньше mid и нижняя граница отсносительно mid + 1 меньше вверхнкй границы
+		if (arr[i] < arr[j]) {
+			c[k] = arr[i]; //записываем в основном массив меньшее значение пары
+			k++; // увеличиваем индекс на 1
+			i++; // сдвигаем нижнюю границу на 1 вправо
+		}
+		else {
+			c[k] = arr[j];
+			k++;//увеличиваем индекс на 1
+			j++;// сдвигаем второй элемент пары вправо
+		}
+	}
+	while (i <= mid and j > high) { // для нечётного количества элементов изначального массива
+		c[k] = arr[i];
+		k++;
+		i++;
+	}
+	for (i = low; i < k; i++) {
+		arr[i] = c[i]; //заносим значения в глобальный массив arr2
+	}
+}
 int main() {
 	setlocale(LC_ALL, "Russian");
-	int quest;
+	const int max_size = 1000;
+	// сортировка пузырьком;
+	int arr[1000], n, counter = 0, k, choice = 0;
 	while (true) {
-		cout << "Чтобы совершить сортировку массвива пузырьком, нажмите 1. \n Чтобы совершить сортировку массива подсчетом нажмите 2.\n Чтобы совершить сортировку массива слиянием нажмите 3\n Чтобы выйти нажмите 0" << endl;
-		cin >> quest;
-		if (quest == 1) {
-			cout << "Вводите цифры для массива. Чтобы заврешить ввод нажмите -" << endl;
-			//Задание 1 сортировка пузырьком
-			int arr[1000] = {};//создание массива для целых чисел
-			int i = 0;
-			while (true) {  //ввод целых чисел	
-				std::string num;
-				cin >> num;
-				arr[i] = atoi(num.c_str());
-				i++;
-				if (num == "-") {
-					break;
-				}
+		std::cout << "Что вы хотите выполнить? \n"
+			<< "1. Сортировка пузырьком\n"
+			<< "2. Сортировка подсчётом\n"
+			<< "3. Сортировка слиянием\n"
+			<< "4. выход\n";
+		std::cin >> choice;
+		switch (choice) {
+		case 1: {
+			std::cout << "Сортировка пузырьком" << std::endl;
+			std::cout << "Введите размера массива n < 1000: ";
+			std::cin >> k;
+			for (int i = 0; i < k; i++) {
+				std::cin >> arr[i];
 			}
-			cout << "Массив который вы ввели" << endl;
-			for (int j = 0; j < i - 1; j++) {
-				cout << arr[j] << " ";
-			}// вывод целых чисел
-			cout << endl;
-			bubble(arr, i);
-			cout << "Сортированный массив" << endl;
-			for (int j = 0; j < i - 1; j++) {
-				cout << arr[j] << " ";
-			} //вывод сортированного массива
-			cout << "\n";
-		}
-		else if (quest == 2) {
-			char arr1[1000] = {};
-			int a = 0;
-			cout << "Чтобы прекратить заполнение массива введите 0" << endl;
-			while (true) {  //ввод целых чисел
-				char sym;
-				cin >> sym;
-				arr1[a] = sym;
-				a++;
-				if (sym == '0') {
-					break;
-				}
-			}
-			cout << "Массив который вы ввели" << endl;
-			for (int j = 0; j < a - 1; j++) {
-				cout << arr1[j] << " ";
-			}// вывод целых чисел
-			cout << endl;
-			cout << "Отсортированный массив";
-			counter(arr1, a - 1);
-			for (int j = 0; j < a - 1; j++) {
-				cout << arr1[j] << " ";
-			}// вывод целых чисел
-			cout << endl;
-		}
-		else if (quest == 3) {
-			cout << "Чтобы прекратить заполнение массива введите -" << endl;
-			int arr1[1000] = {};
-			int a = 0;
-			std::string sym;
-			while (true) {  //ввод целых чисел
-				cin >> sym;
-				arr1[a] = std::atoi(sym.c_str());
-				a++;
-				if (sym == "-") {
-					break;
-				}
-			}
-			for (int j = 0; j < a - 1; j++) {
-				cout << arr1[j] << " ";
-			}// вывод целых чисел
-			cout << endl;
-			merge(arr1, a);
-			for (int j = 0; j < a - 1; j++) {
-				cout << arr1[j] << " ";
-			}// вывод целых чисел
-			cout << endl;
-		}
-		else if (quest == 0) {
+			bubbles(arr, k);
 			break;
 		}
-		else {
-			cout << "Такой операции нет. Введите цифру ещё раз";
+		case 2: {
+			// сортировка подсчётом
+			char arr1[1000];
+			int size_alp = 26, pod[26] = { 0 }, q = 0, size;
+			std::cout << "Сортировка подсчётом" << std::endl;
+			std::cout << "Введите размер массива: ";
+			std::cin >> size;
+			for (int i = 0; i < size; i++) {
+				std::cin >> arr1[i];
+			}
+			withcounter(arr1, size);
+			break;
+		}
+			  // сортировка слиянием;
+		case 3: {
+			int arr2[1000], size1;
+			std::cout << "Сортировка слиянием";
+			std::cout << '\n' << "Введите размер массива n < 1000: ";
+			std::cin >> size1;
+			for (int i = 0; i < size1; i++) {
+				std::cin >> arr2[i];
+			}
+			merge_sort(arr2, 0, size1 - 1);
+			for (int i = 0; i < size1; i++) {
+				std::cout << arr2[i] << ' ';
+			}
+			std::cout << '\n';
+			break;
+		}
+		default: {
+			return 0;
+		}
 		}
 	}
 }
